adb3d001b0a22a392614e74db42660819a71e45c
Santiago Libonati <santiago.libonati@gmail.com>
2021-05-21 23:37:54 -0300

    Cambio total a los html

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-const tags = document.querySelectorAll('[data-tab-target]')
-const tabContents = document.querySelectorAll('[data-tab-content]')
+$(document).ready(function () {
 
-
-tabs.forEach(tab => {
-    tab.addEventListener('click', () => {
-        const target = document.querySelector(tab.dataset.tabTarget)
-        tabContents.forEach(tabContent => {
-            tabContent.classList.remove('active')
-        })
-        tab.forEach(tab => {
-            tab.classList.remove('active')
-        })
-        tab.classList.add('active')
-        target.classList.add('active')
-    })
-})
+	$('#barra_menu').on('click','.barra_menu', function(e) {
+		var cual = parseInt(e.target.id.substring(4));
+		$('.barra_menu').removeClass('w3-white');
+		$(this).addClass('w3-white');
+	});
+});;
