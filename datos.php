eafdbf6508511d69d17e992248f570587108b5ee
Monoliso <lucaseri2003@gmail.com>
2021-08-01 19:22:26 -0300

    Pruebas de conectividad a la base de datos

--- datos.php
+++ datos.php
@@ -1,14 +1,38 @@
 <?php
-include "variables.inc";
-$mysqli = new $mysqli($HOST, $USER, $PASS, $BASE) // El server
+// include "variables.ini";
 
-// Verificación de la existencia (o no) del email ingresado
-$res = $mysqli->query("SELECT Orden FROM clientes WHERE Email = '".$_POST['email']."' ");
-if($res->num_rows)
-    echo -1;
-else{
-    $mysqli->query("INSERT INTO clientes (Nombre, Apellido, Telefono, Email) VALUES 
-    ('".$_POST['nombre']."', '".$_POST['apellido']."', '".$_POST['telefono']."', '".$_POST['email']."') ");
+// $connection = mysqli_connect(
+//     "localhost",
+//     "nuevenue",
+//     "nueve990",
+//     "nueve"
+// );
+
+// if ($connection) {
+//     echo "Database is connected";
+// }
+// else {
+//     die('Could not connect: ' . $connection -> connect_error); // Esto aparece, pero no muestra nada, por lo que no hay error???
+// }
+
+$mysqli = new mysqli("3.130.9.8","nuevenue","nueve990", "nueve");
+
+// Check connection
+if ($mysqli -> connect_error) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
 }
+else {
+  echo "Se conectó correctamente a la base de datos???";
+}
+
+// // Verificación de la existencia (o no) del email ingresado
+// $res = mysqli->query("SELECT Orden FROM clientes WHERE Email = '".$_POST['email']."' ");
+// if($res->num_rows)
+//     echo -1;
+// else{
+//     mysqli->query("INSERT INTO clientes (Nombre, Apellido, Telefono, Email) VALUES 
+//     ('".$_POST['nombre']."', '".$_POST['apellido']."', '".$_POST['telefono']."', '".$_POST['email']."') ");
+// }
 
 ?>
--- funciones.js
+++ funciones.js
@@ -1,20 +1,45 @@
-$(document).ready(function(){
+
+$(function() {
+    console.log('JQuery is Working');
     
-    // Registro Email
-    $("#registro").submit(function(e) {
-        e.preventDefault();
-        var data = new FormData(this);
+    // Que capture el imput con el nombre search, y que cuando se escriba arranque la función
+    $('#nombre').keyup( function() { 
+        let nombre = $('#nombre').val() // Desde ese mismo elemento (u objeto?) quiero obtener el valor
         $.ajax({
-            url: 'datos.php',
+            url: 'formulario.php',
             type: 'POST',
-            data: data,
-            contentType: false,
-            processData: false,
-            success: function(ans) {
-                if (ans == -1)
-                    alert('Cliente ya registrado');
-                $(':input').val('');
+            data: { nombre }, // aparentemente esto es lo mismo que {nombre : nombre}. Lo que está pasando es que estás enviando una propiedad (nombre) con el valor nombre
+            success: function(res) {
+                console.log(res);
+                // if (res == -1)
+                //     alert('Cliente ya registrado');
+                // $(':input').val('');
+            },
+            error: function(respuesta) {
+                console.log('Existe un problema en el archivo "formulario.php"');
+                alert(respuesta);
             }
-        });
+        })
     })
-})
+});
+
+// $(document).ready(function(){
+    
+//     // Registro Email
+//     $("#registro").submit(function(e) {
+//         e.preventDefault();
+//         var data = new FormData(this);
+//         $.ajax({
+//             url: 'datos.php',
+//             type: 'POST',
+//             data: data,
+//             contentType: false,
+//             processData: false,
+//             success: function(res) {
+//                 if (res == -1)
+//                     alert('Cliente ya registrado');
+//                 $(':input').val('');
+//             }
+//         });
+//     })
+// })
