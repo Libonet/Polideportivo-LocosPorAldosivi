b4da0f1a01da52082c7ad05ce1e5e4ef4b57a61c
Santiago Libonati <santiago.libonati@gmail.com>
2021-09-30 01:53:29 -0300

    Galeria mejorada, y anda. login estaba listo creo

--- pages/galeria/script.js
+++ pages/galeria/script.js
@@ -1,28 +1,26 @@
-$(function() {
-  function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}    
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
   }
-  
-  var slideIndex = 1;
-  showSlides(slideIndex);
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
 
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
+var slideIndex = 1;
+showSlides(slideIndex);
 
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
-});
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
